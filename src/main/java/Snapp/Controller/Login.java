7cf29023d501c8f5288bff66f7c4ad4f01a97011
Seyyed Hossein Jahromi <jseyyedhossein@gmail.com>
2023-07-11 15:26:34 +0330

    switching between scenes

---
+++ src/main/java/Snapp/Controller/Login.java
@@ -0,0 +1,16 @@
+package Snapp.Controller;
+
+import Snapp.SnapApplication;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
+public class Login {
+    public void openUserLogin() throws IOException {
+        SnapApplication.changeScene("user-login.fxml");
+    }
+    public void openAdminLogin() throws IOException {
+        SnapApplication.changeScene("admin-login.fxml");
+    }
+}
---
+++ src/main/java/Snapp/SnapApplication.java
@@ -0,0 +1,31 @@
+package Snapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
+public class SnapApplication extends Application {
+    public static Stage window;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        window = stage;
+//        FXMLLoader fxmlLoader = new FXMLLoader(SnapApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(FXMLLoader.load(SnapApplication.class.getResource("login.fxml")));
+        window.setTitle("Snapp!");
+        window.setScene(scene);
+        window.show();
+    }
+    public static void changeScene(String FXMLName) throws IOException {
+        SnapApplication.window.setScene(new Scene(FXMLLoader.load(SnapApplication.class.getResource(FXMLName))));
+        window.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
