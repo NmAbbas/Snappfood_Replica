44579b19b7eebf41fe6b9398665eb4071c0d4558
Seyyed Hossein Jahromi <jseyyedhossein@gmail.com>
2023-07-11 22:54:11 +0330

    modified graphics

--- src/main/java/Snapp/Controller/RestaurantsList.java
+++ src/main/java/Snapp/Controller/RestaurantsList.java
@@ -16,11 +16,10 @@ public class RestaurantsList implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        String a = "";
         if (Restaurant.getRestaurantList().size() != 0) {
             gridPane.setPrefHeight(105*Restaurant.getRestaurantList().size());
             Button[] buttons = new Button[Restaurant.getRestaurantList().size()];
-            //
+            //Restaurant 0
             buttons[0] = new Button(Restaurant.getRestaurantList().get(0).getName());
             buttons[0].setPrefWidth(480);
             buttons[0].setPrefHeight(100);
@@ -30,22 +29,18 @@ public class RestaurantsList implements Initializable {
             buttons[0].setGraphic(imageView);
             buttons[0].setGraphicTextGap(buttons[0].getWidth()-80-buttons[0].getText().length()-20);
             gridPane.add(buttons[0],0,0);
-            for (int i = 0; i < Restaurant.getRestaurantList().size(); i++){
+            //Restaurant 1->
+            for (int i = 1; i < Restaurant.getRestaurantList().size(); i++){
                 buttons[i] = new Button(Restaurant.getRestaurantList().get(i).getName());
                 buttons[i].setPrefWidth(480);
                 buttons[i].setPrefHeight(100);
                 imageView = new ImageView(new Image(SnapApplication.class.getResourceAsStream(Restaurant.getRestaurantList().get(i).getImageURL())));
+                imageView.setFitWidth(80);
+                imageView.setFitWidth(80);
                 buttons[i].setGraphic(imageView);
+                buttons[i].setGraphicTextGap(buttons[i].getWidth()-80-buttons[i].getText().length()-20);
+                gridPane.addRow(i,buttons[i]);
             }
         }
-        Button b = new Button("رستوران اکبر");
-        b.setPrefWidth(480);
-        b.setPrefHeight(100);
-        ImageView i = new ImageView(new Image(SnapApplication.class.getResourceAsStream("/images/hamburger.png")));
-        i.setFitWidth(80);
-        i.setFitWidth(80);
-        b.setGraphic(i);
-        b.setGraphicTextGap(50);
-        gridPane.add(b,0,0);
     }
 }
