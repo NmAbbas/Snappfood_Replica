672ed8394b95137167060343284fcc442ab8dce1
RezaNayeb-H <rezanayebhabib@gmail.com>
2023-07-16 00:03:05 +0330

    some changes

--- src/main/java/Snapp/Controller/AdminNewRestaurant.java
+++ src/main/java/Snapp/Controller/AdminNewRestaurant.java
@@ -5,19 +5,39 @@ import Snapp.FoodType;
 import Snapp.Restaurant;
 import Snapp.SnappApplication;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 
 public class AdminNewRestaurant {
+    public Stage window;
     public TextField foodTypeField;
     public TextField nameField;
     public TextField addressField;
+    public File imgFile = null;
+    public ImageView imgPreview;
 
     public void addRestaurant() {
         try {
             Restaurant r =Restaurant.createRestaurant(nameField.getText(),FoodType.parse(foodTypeField.getText()), Admin.getActiveUser(),Integer.parseInt(addressField.getText()));
-            r.setImageURL();
+            if(imgFile != null)
+            {
+                System.out.println("1----" + imgFile.toPath());
+                File myImg = new File(SnappApplication.class.ge.toString());
+                System.out.println("2-----" + SnappApplication.class.getResourceAsStream(r.toString()).toString());
+                Files.copy(imgFile.toPath(),myImg.toPath());
+                r.setImageURL(SnappApplication.class.getResourceAsStream("/images/" + r.toString()).toString());
+            }
+            else
+                r.setImageURL("images/hamburger.png");
             SnappApplication.changeScene("admin-home.fxml");
 
         } catch (FoodType.UnknownType | IOException e) {
@@ -27,5 +47,13 @@ public class AdminNewRestaurant {
 
     public void openAdminHome() throws IOException {
         SnappApplication.changeScene("admin-home.fxml");
+    }
+
+    public void openFileChooser()
+    {
+        imgFile = null;
+        FileChooser fileChooser = new FileChooser();
+        imgFile = fileChooser.showOpenDialog(window);
+        imgPreview.setImage(new Image(imgFile.getAbsolutePath()));
     }
 }
