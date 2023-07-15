db48a1f9f70a60a69239121cc9351ba6e579ed98
Abbas <abbasnaimi40@gmail.com>
2023-07-15 20:33:51 +0330

    phase 1 comments and responses had a few bugs

--- src/main/java/Snapp/Controller/Login.java
+++ src/main/java/Snapp/Controller/Login.java
@@ -1,14 +1,23 @@
 package Snapp.Controller;
 
+import Snapp.Account;
 import Snapp.SnapApplication;
+import javafx.fxml.Initializable;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Login {
+public class Login implements Initializable {
     public void openUserLogin() throws IOException {
         SnapApplication.changeScene("user-login.fxml");
     }
     public void openAdminLogin() throws IOException {
         SnapApplication.changeScene("admin-login.fxml");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
